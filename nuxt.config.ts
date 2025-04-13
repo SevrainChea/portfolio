afff6f330e828985bdc954452f5f795e97a1675e
SevrainChea <sevrain.chea@gmail.com>
2025-04-13 23:31:38 +0200

    fix: do not use cdn tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      script: [{ src: 'https://cdn.tailwindcss.com?plugins=typography' }],
-    },
-  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
